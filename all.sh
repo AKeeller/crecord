fe4fa0d1bb47c582150db6f096f3faf7b5da6c28
Alberto Amoruso <alberto.am@me.com>
2018-04-25 21:54:56 +0200

    Updated all.sh to be a powerful example

--- all.sh
+++ all.sh
@@ -1,6 +1,15 @@
-IPs=( 192.168.0.9{1..8} )
+ declare -A cameras=(
+	["cancellone"]="192.168.0.91"
+	["strada"]="192.168.0.92"
+	["bagno azzurro"]="192.168.0.93"
+	["terrazzo"]="192.168.0.94"
+	["garage"]="192.168.0.95"
+	["bagno rosa"]="192.168.0.96"
+	["ingresso"]="192.168.0.97"
+	["salone"]="192.168.0.98"
+)
 
-for IP in ${IPs[*]}
+for name in ${!cameras[*]}
 do
-	./script.sh -q $IP &
+	./script.sh -q -c -d "../$name" ${cameras[${name}]} &
 done
