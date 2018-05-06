ddf0e73094b03c5205762cc29b9ea7b4efff89c9
Alberto Amoruso <alberto.am@me.com>
2018-05-06 18:50:40 +0200

    all.sh now uses auto_ssn

--- all.sh
+++ all.sh
@@ -10,34 +10,9 @@ declare -A cameras=(
 	["salone"]="192.168.0.98"
 )
 
-segment_start_number=0
-
-# A POSIX variable
-OPTIND=1    # Reset in case getopts has been used previously in the shell.
-
-while getopts ":s:" opt; do
-    case "$opt" in
-    s)
-        segment_start_number=$OPTARG
-        ;;
-    :)
-        echo "Option -$OPTARG requires an argument." >&2
-        exit 1
-        ;;
-    \?)
-        echo -e "${BOLD}-$OPTARG${NORMAL} is not a valid argument." >&2
-        exit 1
-        ;;
-    esac
-done
-
-shift $((OPTIND-1))
-
-[ "$1" = "--" ] && shift
-
 for name in "${!cameras[@]}"
 do
-	./record.sh -q -t 1800 -s $segment_start_number -c -d "../$name" "${cameras[${name}]}" &
+	./record.sh -q -t 1800 -c -d "../$name" "${cameras[${name}]}" &
 done
 
 sleep 1
