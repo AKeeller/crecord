540c8efdb749cd739d0c7a3576cb7278281e5c37
Alberto Amoruso <alberto.am@me.com>
2018-05-15 17:37:09 +0200

    Added ascii art to install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
 readonly DESTINATION="/usr/local/bin/crecord"
 
 if [ -f helper.sh ]; then
@@ -15,3 +20,7 @@ fi
 install -v record.sh crecord.sh delete.sh all.sh helper.sh "$DESTINATION"
 
 ln -sfv "$DESTINATION/crecord.sh" /usr/local/bin/crecord.sh
+
+if [ -f ascii.txt ]; then
+	cat ascii.txt
+fi
