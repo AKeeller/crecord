63996a3c6b9635f16177e13a3ac5fc22185c8eba
Alberto Amoruso <alberto.am@me.com>
2018-05-15 18:45:56 +0200

    Updated all.sh

--- all.sh
+++ all.sh
@@ -9,12 +9,13 @@ source /etc/crecord.config
 
 if [ -z "$destination_folder" ]; then
 	echo "destination_folder is unset"
+	echo "set it in /etc/crecord.config"
 	exit 1
 fi
 
 for name in "${!cameras[@]}"
 do
-	./record.sh -l -L -t 1800 -c -d "$destination_folder/$name" "${cameras[${name}]}" &
+	/usr/local/bin/crecord.d/record.sh -l -L -t 1800 -c -d "$destination_folder/$name" "${cameras[${name}]}" &
 done
 
 sleep 20
