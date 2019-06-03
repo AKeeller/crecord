8979dfc5b7f3cbf816ec80e024654dcb4f7572a3
Alberto Amoruso <alberto.am@me.com>
2019-06-03 16:55:09 +0200

    crecord all is now deprecated, replaced by the init.d script

--- crecord.sh
+++ crecord.sh
@@ -10,14 +10,15 @@ case $1 in
 		command="/usr/local/bin/crecord.d/delete.sh"
 		;;
 	all)
-		command="/usr/local/bin/crecord.d/all.sh"
+		echo -e "crecord all is now deprecated. Use\n\n\tsudo /etc/init.d/crecord start\n\ninstead. If you want anyway to manually start crecord all, use\n\n\t/usr/local/bin/crecord.d/all.sh"
+		exit 0
 		;;
 	uninstall)
 		echo -e "Uninstall with:\n\tsudo /etc/init.d/crecord uninstall"
 		exit 0
 		;;
 	*)
-		echo "Usage: `basename $0` [record | delete | all | uninstall]"
+		echo "Usage: `basename $0` [record | delete]"
 		exit 1
 esac
 
