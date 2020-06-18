4e27a160eeec803528b4e10e0c0a2b5125e1680d
Alberto Amoruso <alberto.am@me.com>
2020-06-18 22:44:54 +0200

    Removed any reference to the init.d script

--- install.sh
+++ install.sh
@@ -24,11 +24,8 @@ fi
 
 
 install -v -m 755 record.sh crecord.sh delete.sh all.sh helper.sh uninstall.sh "$DESTINATION"
-install -v -m 755 crecord.init /etc/init.d/crecord
 install -v -m 644 bash_completion /etc/bash_completion.d/crecord
 install -v -m 644 -D crecord.1 /usr/local/share/man/man1/
-
-update-rc.d crecord defaults
 
 mandb &> /dev/null
 
--- uninstall.sh
+++ uninstall.sh
@@ -15,15 +15,11 @@ fi
 echo "Uninstalling crecord $(print_version)"
 confirm
 
-/etc/init.d/crecord stop
-
 rm -rfv /usr/local/bin/crecord*
 rm -rfv /var/log/crecord
 rm -fv /etc/bash_completion.d/crecord
 rm -fv  /var/run/crecord.pid
-rm -fv  /etc/init.d/crecord
 rm -fv /usr/local/share/man/man1/crecord.1
-update-rc.d -f crecord remove
 
 echo "Do you want to uninstall /etc/crecord.config?"
 confirm
